ad918661c16ca7f281bb62beac487fa822ae8938
Samyog <pantasamyog@gmail.com>
2022-11-13 22:04:46 -0600

    extension complete

--- ChromeExtension/enxtension.js
+++ ChromeExtension/enxtension.js
@@ -1,37 +1,75 @@
 let myLeads = [] 
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const saveBtn = document.getElementById("save-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
+
+if (leadsFromLocalStorage ){
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+
+
+function render(leads){
+    let listItems = ""
+     for (let i = 0; i<  leads.length; i++){
+    
+     
+            listItems += `
+            <li>
+            <a  target='_ blank' href='${leads[i]}' >
+                 ${leads[i]}
+                 </a>
+            </li> 
+            `
+    
+       console.log(listItems)
+    
+     }
+     ulEl.innerHTML = listItems
+
+    }
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    renderLeads()
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
+    render(myLeads)
+
+
 })
 
-function renderLeads(){
-let listItems = ""
- for (let i = 0; i<  myLeads.length; i++){
+deleteBtn.addEventListener("dblclick", function(){
+
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
 
 
-   // listItems += "<li><a  target='_ blank' href='" + myLeads[i] +"' > " + myLeads[i] + "</a></li> " 
+ })
+
+ saveBtn.addEventListener("click", function(){
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify( myLeads))
+        render(myLeads)
+    })
+
+
     
-        listItems = `
-        <li>
-        <a  target='_ blank' href='${myLeads[i]}' >
-             ${myLeads[i]}
-             </a>
-        </li> 
-        `
+ })
 
-   console.log(listItems)
 
-  /*   const li = document.createElement("li")
-    li.textContent = myLeads[i]
-    ulEl.append(li) */
- }
 
- ulEl.innerHTML = listItems
 
-}
+
+
+
+
